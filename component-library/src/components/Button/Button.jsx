9c7835f1cdc90388ef843849d60cf9cd33b284f5
Sunam Niroula <69410705+NiroulaSunam@users.noreply.github.com>
2026-06-14 16:54:47 +0545

    adding card component

--- component-library/src/components/Button/Button.jsx
+++ component-library/src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import styles from './Button.module.css' // Since we are using CSS Modules, we have to import our styles and acess classes through that import 
 
-function Button({ label, variant = 'primary', size = 'medium', disabled = false }) {
+function Button({ label, variant = 'primary', size = 'medium', disabled = false, onClick }) {
     return (
         <button className={`${styles.button} ${styles[variant]} ${styles[size]} ${disabled ? styles.disabled : ''}`} 
         disabled={disabled}
+        onClick={onClick}
         >   
         {/* here styles.button will always apply. and styles[variant] uses bracket notation to dynamically look up the class. */}
         {/* disable the button from being clicked */}    
